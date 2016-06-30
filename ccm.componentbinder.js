f760a5c8f4345ba202ec7cd46b3dec094ad3c31e
Brahim Kharbech <brahim.kharbech@smail.inf.h-brs.de>
2016-06-30 17:10:38 +0200

    3 Datein .js und .html und .css um 2 Components einzubinden letzte Version

--- ccm.componentbinder.js
+++ ccm.componentbinder.js
@@ -2,24 +2,21 @@
     name: 'componentbinder',
     config: {
       instance_messenger: [ ccm.instance, '../ab3/ccm.messenger.js' ],
-      instance_calc: [ ccm.instance, 'http://johnjonnyjohn.github.io/ccm-components/resources/calculator/calculator.js' ],
-        instance_clock: [ ccm.instance,'http://mkaul.github.io/ccm-components/resources/clock/ccm.clock.js'],
+        instance_item_verwaltung: [ ccm.instance,'http://www2.inf.fh-bonn-rhein-sieg.de/~mwelsc2s/w4/item-verwaltung.js'],
         style: [ccm.load,'./style.css']
         
     },
     Instance: function () {
       this.init = function ( callback ) {
         this.instance_messenger.element = ccm.helper.find( this, '.messenger' );
-        this.instance_clock.element = ccm.helper.find( this, '.clock' );
-        this.instance_calc.element = ccm.helper.find( this, '.calc' );  
+        this.instance_item_verwaltung.element = ccm.helper.find( this, '.item_verwaltung' );
         callback();
       };
       this.render = function ( callback ) {
         var element = ccm.helper.element( this );
-        element.html( '<div class="messenger"></div><div class="clock"></div><div class="calc"></div>' );
+        element.html( '<div class="messenger"></div><div class="item_verwaltung"></div>' );
         this.instance_messenger.render();
-        this.instance_clock.render();
-        this.instance_calc.render();
+        this.instance_item_verwaltung.render();
         if ( callback ) callback();
       }
     }
